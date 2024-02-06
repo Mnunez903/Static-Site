4c8faf123bffcc8c3a0b731505d6eb279d13abba
Michael Nunez <mnunez903@gmail.com>
2024-02-06 11:44:23 -0500

    Moved around some routing and colors

--- src/Share/NotFound.js
+++ src/Share/NotFound.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PageTitle from "./PageTitle";
+import underConstruction from '../assets/images/under-construction.png' 
 
 const NotFound = () => {
   return (
     <>
       <PageTitle title="404"></PageTitle>
       <div className="h-screen w-full flex flex-col justify-center items-center  bg-homeBg-dark bg-no-repeat bg-center bg-cover bg-fixed">
+        <img className='h-64' src={underConstruction}/>
         <h1 className="text-7xl leading-none text-white font-extrabold md:text-8xl">
           404!
         </h1>
@@ -16,7 +18,7 @@ const NotFound = () => {
 
         <Link
           to="/"
-          className="flex items-center mx-auto bg-gradient-to-r from-[#FA5252] to-[#DD2476] duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD2476]  to-[#fa5252ef] px-8 py-3 text-lg text-white rounded-[5px]"
+          className="flex items-center mx-auto bg-gradient-to-r from-[#ffb16d] to-[#dd8424] duration-200 transition ease-linear hover:bg-gradient-to-l from-[#DD8424]  to-[#ffb16d] px-8 py-3 text-lg text-white rounded-[5px]"
         >
           Back To Home
         </Link>
